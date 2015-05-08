8c5b620c3fc73bf108aa611414465df9e2d6c5ef
Hannes Kindströmmer <me@brolaugh.com>
2015-05-08 21:10:55 +0200

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "brolaugh_me";
-	$password = "kattjävel5";
-	$database = "brolaugh_melodifestivalen";
-	$conn = mysqli_connect($servername, $username, $password, $database);
-	if(!$conn){
-		die("Connection failed: ". mysqli_connect_error());
-	}
